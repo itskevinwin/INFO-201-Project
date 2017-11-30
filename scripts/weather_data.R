8b54281014a6f80433e74b8da850b662f0e3e82b
devon <neuricos@gmail.com>
2017-11-30 15:23:15 -0800

    Revised weather data

--- scripts/weather_data.R
+++ scripts/weather_data.R
@@ -1,32 +1,89 @@
+# file weather_data.R
+
 library(jsonlite)
 library(dplyr)
 library(httr)
 
+# Source api key
 source('scripts/api_key.R')
 
-main.cities <- read.csv('data/main_cities.csv')
+# Read in main_cities.csv file
+main.cities <- read.csv(
+  'data/main_cities.csv',
+  stringsAsFactors = FALSE
+)
 
-urlFunction <- function(city){
-  base.url <- 'http://api.openweathermap.org/data/2.5/weather?q='
-  full.url <- paste0(base.url, city)
-  query.params <- list(APPID = weather.key)
-  response <- GET(full.url, query = query.params)
-  results <- content(response, "text") %>% fromJSON(body)
-  results <- data.frame(results)
-  return(results)
+main.cities.data <- CreateMainCitiesDF(main.cities$main.cities)
+
+AdjustTime <- function(str) {
+  return(as.POSIXct(str, origin="1970-01-01", tz="GMT"))
 }
 
-city.data <- data.frame()
-all.cities <- urlFunction(main.cities[1,])
-for(x in 2:50){
-  city.data <- urlFunction(main.cities[x,])
-  if(ncol(city.data) == ncol(all.cities)){
-    all.cities <- rbind(all.cities, city.data)
-  }else{
-    all.cities <- full_join(all.cities, city.data)
+CreateMainCitiesDF <- function(cities) {
+  len = length(cities)
+  cities.df <- GetCityInfo(q = cities[1])
+  for (i in 2:len) {
+    city = cities[i]
+    cities.df <- rbind(cities.df, GetCityInfo(q = city))
+  }
+  return(cbind(cities, cities.df))
+}
+
+FetchResponse <- function(full.uri, params) {
+  return(
+    GET(full.uri, query = params) %>% content('text') %>% fromJSON()
+  )
+}
+
+FetchValue <- function(val) {
+  return(ifelse(is.null(val), "Unknown", val))
+}
+
+GetCityInfo <- function(q = NULL, lat = NULL, lon = NULL, zip = NULL) {
+  base.uri <- 'http://api.openweathermap.org/data/2.5/weather?'
+  query.params <- list(APPID = weather.key)
+  response <- NULL
+  if (is.null(q) & (is.null(lat) | is.null(lon)) & is.null(zip)) {
+    return('Invalid query parameters')
+  } else {
+    if (!is.null(q)) {
+      response <- FetchResponse(paste0(base.uri, 'q=', q), query.params)
+    } else if (!is.null(lon) & !is.null(lat)) {
+      response <- FetchResponse(
+        paste0(base.uri, 'lat=', lat, '&lon=', lon),
+        query.params
+      )
+    } else if (!is.null(zip)) {
+      response <- FetchResponse(
+        paste0(base.uri, 'zip=', zip),
+        query.params
+      )
+    }
+    return(ProcessResponse(response))
   }
 }
 
-cities.data <- subset(all.cities,select = c('coord.lon', 'coord.lat', 'weather.main', 'weather.description', 'main.temp', 'main.pressure', 'main.humidity',
-                           'main.temp_min', 'main.temp_max', 'visibility', 'wind.speed', 'wind.deg', 'all', 'dt', 'sys.sunrise',
-                           'sys.sunset', 'name'))
+ProcessResponse <- function(response) {
+  return(data.frame(
+    lon = FetchValue(response$coord$lon),
+    lat = FetchValue(response$coor$lat),
+    weather = FetchValue(response$weather$main[1]),
+    description = FetchValue(response$weather$description[1]),
+    temp = FetchValue(response$main$temp),
+    temp.min = FetchValue(response$main$temp_min),
+    temp.max = FetchValue(response$main$temp_max),
+    pressure = FetchValue(response$main$pressure),
+    humidity = FetchValue(response$main$humidity),
+    visibility = FetchValue(response$visibility),
+    wind.speed = FetchValue(response$wind$speed),
+    wind.degree = FetchValue(response$wind$deg),
+    rain = FetchValue(response$rain$rain.3h),
+    snow = FetchValue(response$snow$snow.3h),
+    cloudiness = FetchValue(response$clouds$all),
+    time = FetchValue(AdjustTime(response$dt)),
+    country = FetchValue(response$sys$country),
+    sunrise = FetchValue(AdjustTime(response$sys$sunrise)),
+    sunset = FetchValue(AdjustTime(response$sys$sunset)),
+    city = FetchValue(response$name)
+  ))
+}
